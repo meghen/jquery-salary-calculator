14d7151387ce6fcb5544fbde088709ef3691c1b0
Megan Henry <megangabriellehenry@gmail.com>
2020-02-03 11:19:03 -0600

    add color alternating in table

--- scripts/client.js
+++ scripts/client.js
@@ -23,42 +23,44 @@ function calculateCosts(salaryIn) {
     }//end if
 }//end calculateCosts
 
-
-
 function informationIn() {
+    //set variables to jquery info for cleaner code later
     let firstName = $('#firstNameIn').val();
     let lastName = $('#lastNameIn').val();
     let idNum = $('#idIn').val();
     let jobTitle = $('#jobTitleIn').val();
     let annualSalary = $('#annualSalaryIn').val();
-    
+    //declare object with parameters whose value is jquery code above
     let employeeInfo = {
         name: firstName +' '+ lastName,
         ID: idNum,
         job: jobTitle,
         salary: annualSalary
-    };
+    };//end obj
+    //clear fields
     $('#firstNameIn').val('');
     $('#lastNameIn').val('');
     $('#idIn').val('');
     $('#jobTitleIn').val('');
     $('#annualSalaryIn').val('');
-
+    //append jq input vals to employee table. it has to go row by row, instead of column by column because of the directionality of html/jq. 
+    /// BUTTON NOT RECOGNIZING ID, THUS WONKING UP LINE 54
     $('#employeeTable').append(`<tr><td>${firstName}</td><td>${lastName}</td><td>${idNum}</td><td>${jobTitle}</td><td>${annualSalary}</td><td><button id="${idNum}">Delete</button></td></tr>`);
-
+    //push newly created employee object into global employees array
     employees.push(employeeInfo);
+    //run calculateCosts fx calling the specific employeeInfo obj property 'salary'.
     calculateCosts(employeeInfo.salary);
+    //add the listener fx w/in this add employee fx. it won't run until the deleteButton is clicked
     $('#employeeTable').on('click', `#${idNum}`, deleteEmployee);
 
     function deleteEmployee() {
+        //removes whole row of cells (<tr></tr>) from DOM
         let el = $(this);
         let string = el.parent();
         let grandparent = string.parent()[0];
         grandparent.remove();
-    
+        //call calculateCosts fx inputting annualSalary(-1) run annualSalary as neg because adding a neg and subtracting a pos are the same
+        //ALSO be sure not to confuse an individuals annualSalary with the globally declared, company's annualCosts
        calculateCosts(annualSalary*(-1));
-    }//end deleteEmployee
-}//end informationIn
-
-
-//For Base mode, it does **not** need to remove that Employee's salary from the reported total.
+    };//end deleteEmployee
+};//end informationIn
